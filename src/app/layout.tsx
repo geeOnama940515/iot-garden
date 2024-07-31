ce2f995346f8a222481394071b19b30f9fed1697
Gregorio Amano III <66071367+geeOnama940515@users.noreply.github.com>
2024-07-31 14:07:06 +0800

    fixed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IoT Graden",
+  title: "Grenhouse|Demo",
   description: "Call me Master GREG",
 };
 
